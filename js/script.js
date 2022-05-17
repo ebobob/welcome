9a4a9cab112e4d24dba8152c164231069dcba4a8
sergey <pochtame@outlool.com>
2022-05-17 15:58:39 +0500

    add main

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 1000,
+		Infinity: true,
+		autoplay: true,
+		autoplaySpeed: 8000,
+		fade: true,
+	});
+});
